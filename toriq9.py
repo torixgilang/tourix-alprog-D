332e99c60db108fb94f4f0397466609b0fd804e4
torixgilang <tourixgilang@gmail.com>
2024-12-15 12:24:42 +0800

    Add files via upload

---
+++ toriq9.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 37
+# suhu dalam fahrenheit = 98.6
+# suhu dalam kelvin = 310.15
+# suhu dalam reamur = 29.6
+
+# suhu dalam celcius
+celcius = 37
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
